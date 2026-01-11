e8dfef54c306d4087359cfc316be53e145992ed1
shaharlevi25 <shahale3@ac.sce.ac.il>
2026-01-11 11:36:56 +0200

    מערכת תשלום

--- Controllers/PaymentController.cs
+++ Controllers/PaymentController.cs
@@ -1,10 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using TripsProject.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace TripsProject.Controllers
 {
     public class PaymentController : Controller
-    
     {
+        private readonly string _connectionString;
+
+        public PaymentController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("TravelDb");
+        }
+
         public IActionResult Success()
         {
             ViewBag.Msg = "התשלום בוצע בהצלחה (Sandbox)";
@@ -16,9 +25,35 @@ namespace TripsProject.Controllers
             ViewBag.Msg = "התשלום בוטל";
             return View();
         }
-        public IActionResult Checkout()
+        public IActionResult Checkout(int packageId)
         {
-            return View();
+            TravelPackage package = null;
+
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                conn.Open();
+
+                string sql = "SELECT * FROM TravelPackages WHERE PackageId = @PackageId";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@PackageId", packageId);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    package = new TravelPackage
+                    {
+                        PackageId = (int)reader["PackageId"],
+                        Destination = reader["Destination"].ToString(),
+                        Country = reader["Country"].ToString(),
+                        Price = (decimal)reader["Price"]
+                    };
+                }
+            }
+
+            if (package == null)
+                return NotFound("Package not found");
+
+            return View(package);
         }
     }
 }
