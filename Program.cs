05f379fab420ee525b97c1f3661fa19e221d6a65
shaharlevi25 <shahale3@ac.sce.ac.il>
2026-01-11 20:17:22 +0200

    Remove secrets and use user-secrets

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 using TripsProject.Data;
 using TripsProject.Services;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
